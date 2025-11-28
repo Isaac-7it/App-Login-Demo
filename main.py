8286e00721ba52ee64b6648f747aabdd2ebbe451
dev AIO <isaacmail.07@gmail.com>
2025-11-28 13:51:12 +0100

    bug finally fixed 😫

--- main.py
+++ main.py
@@ -14,13 +14,12 @@ while running:
         new_user_email = input('Email ')
         new_user_password = input('Enter password ')
         new_user_confirm_password = input('Confirm password ')
-        new_user = User(new_username, new_user_email, new_user_password)
         is_email_valid = '@gmail.com' in new_user_email
         # Validating Emails
-        # while not is_email_valid:
-        #     new_user_email = input('Your Email should end with a domain! Email ')
-        #     if '@gmail.com' in new_user_email:
-        #         break
+        while not is_email_valid:
+            new_user_email = input('Your Email should end with a domain! Email ')
+            if '@gmail.com' in new_user_email:
+                break
         if new_user_confirm_password == new_user_password:
             command = input('Account created! Proceed to Login (L) ')
         while new_user_confirm_password != new_user_password:
@@ -29,17 +28,16 @@ while running:
 
             if new_user_confirm_password == new_user_password:
                 command = input('Account created! Proceed to Login (L) ')
+        new_user = User(new_username, new_user_email, new_user_password)
     elif command.lower() == 'login' or command.lower() == 'l':
         try:
             login_email = input('Email ')
             login_password = input('Password ')
-            if login_email == new_user.email and login_password == new_user_password:
+            if login_email == new_user.email and login_password == new_user.password:
                 print(f'Login Successful! Welcome {new_user.name}')
                 running = False
                 break
             else:
-                # print(login_email, new_user_email)
-                # print(login_password, new_user_password)
                 print('Wrong Email or Password')
         except NameError:
             command = input("Your account doesn't exist. Create an Account (C) ")
